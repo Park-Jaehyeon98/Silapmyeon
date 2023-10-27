ecf8647e2729a257b7ee7e63e2f9d10ad714ea16
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-10-27 15:39:04 +0900

    feat: report 도메인과 관련된 도메인 객체 생성

---
+++ BE/interview/src/main/java/com/b107/interview/domain/report/entity/Interview.java
@@ -0,0 +1,10 @@
+package com.b107.interview.domain.report.entity;
+
+import lombok.Getter;
+
+@Getter
+public class Interview {
+
+    private String question;
+    private String answer;
+}
--- BE/interview/src/main/java/com/b107/interview/domain/report/entity/Report.java
+++ BE/interview/src/main/java/com/b107/interview/domain/report/entity/Report.java
@@ -1,4 +1,19 @@
 package com.b107.interview.domain.report.entity;
 
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Document(collation = "reports")
 public class Report {
+
+    @Id
+    private String id;
+    private List<Interview> Interviews = new ArrayList<>();
+    private int eyeTrackingViolationCount;
+
 }
