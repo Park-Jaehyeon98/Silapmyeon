bf3e4ace4e0c6e40a12a0ba14e2e61b4bef6a14f
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-10-30 15:36:55 +0900

    refactor: ReportRepository findByUserId 메서드 return type 변경

--- BE/interview/src/main/java/com/b107/interview/domain/report/repository/ReportRepository.java
+++ BE/interview/src/main/java/com/b107/interview/domain/report/repository/ReportRepository.java
@@ -4,12 +4,12 @@ import com.b107.interview.domain.report.entity.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ReportRepository extends MongoRepository<Report, String> {
 
-    Optional<Report> findByUserId(Long userId);
+    List<Report> findByUserId(Long userId);
 
-    boolean existsByUserId(Long userId);
+    boolean existsUserByUserId(Long userId);
 }
