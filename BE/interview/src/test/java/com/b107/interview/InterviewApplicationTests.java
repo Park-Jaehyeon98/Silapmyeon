c481442d6729eb8831ebf31137d6082d45ef90de
박재현 <10225-holizon9@users.noreply.lab.ssafy.com>
2023-10-30 15:41:47 +0900

    Fix : @test 주석처리

--- BE/interview/src/test/java/com/b107/interview/InterviewApplicationTests.java
+++ BE/interview/src/test/java/com/b107/interview/InterviewApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InterviewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
