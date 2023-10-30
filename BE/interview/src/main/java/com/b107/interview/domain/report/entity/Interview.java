70e23deeaf5d9e8cbeecb283c8a431aa690527b5
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-10-30 09:53:40 +0900

    feat: Interview 팩토리 메서드 추가

--- BE/interview/src/main/java/com/b107/interview/domain/report/entity/Interview.java
+++ BE/interview/src/main/java/com/b107/interview/domain/report/entity/Interview.java
@@ -1,10 +1,18 @@
 package com.b107.interview.domain.report.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 
-@Getter
+@Getter @Builder
 public class Interview {
 
     private String question;
     private String answer;
+
+    public static Interview of(String question, String answer) {
+        return Interview.builder()
+                .question(question)
+                .answer(answer)
+                .build();
+    }
 }
