7c4a8c15445a6c55a81681e5d13c3dbdfa69cc20
안도희 <adh1120@naver.com>
2023-11-02 14:04:40 +0900

    components 위치 설정

--- FE/silapmyeon/src/App.js
+++ FE/silapmyeon/src/App.js
@@ -2,15 +2,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-//import './App.css';
+import './App.css';
 import Sidebar from './components/commons/sidebar';
 import Header from "./components/commons/Header";
+import Home from "./pages/Home/Home"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
+      <div className='main'>
+        <Sidebar />
+        <div className='page'>
+          <Home/>
+        </div>
+      </div>
     </div>
   );
 }
