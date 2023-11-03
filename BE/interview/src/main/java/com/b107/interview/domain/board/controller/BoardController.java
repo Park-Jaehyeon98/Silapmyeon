41b248effd6443e7b6fa6858d125f26555c5ba2e
박영기 <rl0428@naver.com>
2023-11-03 17:06:18 +0900

    Feat: 커뮤니티 게시글 페이지네이션 적용

--- BE/interview/src/main/java/com/b107/interview/domain/board/controller/BoardController.java
+++ BE/interview/src/main/java/com/b107/interview/domain/board/controller/BoardController.java
@@ -6,6 +6,10 @@ import com.b107.interview.domain.board.service.BoardService;
 import com.b107.interview.domain.report.dto.response.ReportResponse;
 import com.b107.interview.domain.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,11 +35,9 @@ public class BoardController {
 
     //게시판 글 전체 조회
     @GetMapping("/boards")
-    public ResponseEntity<List<BoardAllResponse>> findAllBoards(){
-        List<BoardAllResponse> boards = boardService.findAll()
-                .stream()
-                .map(BoardAllResponse::new)
-                .collect(Collectors.toList());
+    public ResponseEntity<Page<BoardAllResponse>> findAllBoards(@PageableDefault(sort = "boardId", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<BoardAllResponse> boards = boardService.findAll(pageable)
+                .map(BoardAllResponse::new);
 
         return ResponseEntity.ok()
                 .body(boards);
