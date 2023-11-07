9585afbaa8d9ff48efc883e5cbe62d7fa2e0bf27
박영기 <rl0428@naver.com>
2023-11-07 10:21:38 +0900

    Modify: axios 설정 수정

--- FE/silapmyeon/src/api/api.js
+++ FE/silapmyeon/src/api/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const jwt = {
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjo5LFwidXNlckVtYWlsXCI6XCJva2lwMDQyOEBnbWFpbC5jb21cIixcInJvbGVcIjpcIlJPTEVfVVNFUlwiLFwidHlwZVwiOlwiQVRLXCJ9IiwiaWF0IjoxNjk5Mjg5OTQwLCJleHAiOjE3MDA0OTk1NDB9.eg0GtDJfzc3xZpkwb7C6NRsY0DThe6hmTIzLhyNkga4",
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjo5LFwidXNlckVtYWlsXCI6XCJva2lwMDQyOEBnbWFpbC5jb21cIixcInJvbGVcIjpcIlJPTEVfVVNFUlwiLFwidHlwZVwiOlwiQVRLXCJ9IiwiaWF0IjoxNjk5MzE5MzE3LCJleHAiOjE3MDA1Mjg5MTd9.lEnScHqHYfTPgwbLH_TAA8PViRf1aZtC-DTc67xHAwk",
 };
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://k9b107a.p.ssafy.io/api",
   headers: jwt,
 });
 
