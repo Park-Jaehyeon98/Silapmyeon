5963dd0d6dc7727fb049f63374d9841bb79b6d7d
박재현 <10225-holizon9@users.noreply.lab.ssafy.com>
2023-11-08 21:27:20 +0900

    Feat : seeso 개발 중 화면 안나옴

--- FE/silapmyeon/src/components/interview/Eyetracking.jsx
+++ FE/silapmyeon/src/components/interview/Eyetracking.jsx
@@ -1,53 +1,84 @@
-// Import necessary dependencies
-import React, { useEffect, useState } from "react";
-import webgazer from "webgazer";
-import styles from "./Eyetracking.module.css"; // Import the CSS module
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import EasySeeso from "seeso/easy-seeso";
+import Webcam from "react-webcam";
+import styled from "styled-components";
 
-function EyeTracking() {
-  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+const WaitScreen = styled.div`
+  width: 100%;
+  height: calc(100vh - 6.75rem);
+  border-radius: 1.25rem;
+  margin-bottom: 0.75rem;
+  overflow: hidden;
+  background-color: #e1e6f9;
+  position: relative;
+`;
 
-  const updateCoordinates = (data) => {
-    if (data) {
-      const { x, y } = data;
-      setCoordinates({ x, y });
-    }
-  };
+const Eyetracking = () => {
+  const webcamRef = useRef(null);
+  const seesoRef = useRef(null);
 
   useEffect(() => {
-    // Initialize WebGazer
-    webgazer
-      .init()
-      .then(() => {
-        // Set up a gaze listener
-        webgazer.setGazeListener(updateCoordinates);
+    async function initializeSeeso() {
+      seesoRef.current = new EasySeeso();
 
-        // Additional code to load the model
-        webgazer
-          .setTracker("TFFaceMesh")
-          .setGazePrediction((prediction) => {
-            // Do something with the gaze prediction data if needed
-          });
+      // Seeso 초기화 및 설정
+      await seesoRef.current.init(
+        "라이센스 키 넣기",
+        () => {
+          // Seeso 초기화 성공 콜백
+          console.log("Seeso initialized successfully.");
+        },
+        () => {
+          // Seeso 초기화 실패 콜백
+          console.error("Seeso initialization failed.");
+        }
+      );
 
-        // Start WebGazer
-        webgazer.begin();
-      })
-      .catch((err) => {
-        console.error("WebGazer initialization error:", err);
-      });
+      // Seeso 시작 (웹캠을 사용하기 전에 Seeso를 시작해야 함)
+      await seesoRef.current.startTracking(
+        (gazeInfo) => {
+          // 눈의 움직임을 추적하는 콜백
+          console.log("Gaze Info:", gazeInfo);
+        },
+        (FPS, latency_min, latency_max, latency_avg) => {
+          // 디버그 정보 콜백
+          console.log("FPS:", FPS);
+          console.log("Latency (min/max/avg):", latency_min, latency_max, latency_avg);
+        }
+      );
+    }
 
-    return () => {
-      webgazer.end();
-    };
+    initializeSeeso();
   }, []);
 
   return (
-    <div className={styles["eye-tracking"]}>
-      <h2 className={styles["heading"]}>Eye Tracking Component</h2>
-      <p>Open the webcam to track your eye movements.</p>
-      <p>Gaze X: {coordinates.x}</p>
-      <p>Gaze Y: {coordinates.y}</p>
-    </div>
+    <>
+      <WaitScreen>
+        <canvas
+          id="output"
+          style={{
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            zIndex: 9999,
+          }}
+        />
+        {webcamRef.current && (
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "auto",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          />
+        )}
+      </WaitScreen>
+    </>
   );
-}
+};
 
-export default EyeTracking;
+export default Eyetracking;
