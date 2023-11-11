f9c602acf5044dc22689cacdbbaad123a33241bf
안도희 <adh1120@naver.com>
2023-11-11 17:21:08 +0900

    [edit] css 수정

--- FE/silapmyeon/src/components/Comment/Comment.jsx
+++ FE/silapmyeon/src/components/Comment/Comment.jsx
@@ -26,6 +26,13 @@ function Comment({ comment }) {
 
   return (
     <div className="comment" style={{ marginBottom: "30px" }}>
+      <div
+        style={{
+          width: "100%",
+          border: "1px solid #d9d9d9",
+          marginBottom: "10px",
+        }}
+      ></div>
       <div className="commentUser">
         <img className="commentImg" src={comment.profileImg}></img>
         <div className="commentNickname">{comment.nickname}</div>
@@ -36,7 +43,6 @@ function Comment({ comment }) {
           display: "flex",
           marginLeft: "20px",
           marginTop: "20px",
-          height: "150px",
         }}
       >
         {comment.content}
--- FE/silapmyeon/src/pages/Board/BoardDetail.jsx
+++ FE/silapmyeon/src/pages/Board/BoardDetail.jsx
@@ -79,8 +79,10 @@ function BoardDetail() {
         <div className="boardTitle">{board.title}</div>
 
         <div className="boardLine"></div>
+
         <div className="boardInfo">
-          <div className="boardDate">{board.createdTime.substring(0, 10)}</div>
+          <div className="boardUser">{board.nickname}</div>
+          <div className="boardHits">{board.createdTime.substring(0, 10)}</div>
           <div className="boardHits">조회수 {board.hit}</div>
         </div>
         {board.userId === userValue.userId && (
@@ -111,11 +113,15 @@ function BoardDetail() {
           <div className="boardContent">{board.content}</div>
           {report != null && <Report data={report}></Report>}
 
+          <div style={{ marginRight: "800px", marginBottom: "15px" }}>
+            답변 {comments.length}
+          </div>
           <div className="commentContainer">
             <div className="commentRegistUser">
               <img className="registImg" src={userValue.userProfileUrl}></img>
               <div className="registNickname">{userValue.userNickname}</div>
             </div>
+
             <div className="boardComment">
               <div className="commentRegist">
                 <textarea
@@ -141,9 +147,6 @@ function BoardDetail() {
                 <div></div>
               </div>
 
-              <div style={{ marginRight: "800px", marginBottom: "15px" }}>
-                답변 {comments.length}
-              </div>
               <div className="comments">
                 {comments.map((commentData) => (
                   <Comment comment={commentData}></Comment>
