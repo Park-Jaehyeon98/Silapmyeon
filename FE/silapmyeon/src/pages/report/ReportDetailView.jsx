f47a87b527e1c30b1d79584fabcc7019147b3473
김현수 <ekclstkfka5@gmail.com>
2023-11-12 22:29:13 +0900

    feat: 레포트 삭제 구현

--- FE/silapmyeon/src/pages/report/ReportDetailView.jsx
+++ FE/silapmyeon/src/pages/report/ReportDetailView.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { getReportById } from "../../api/report";
+import { deleteReportById } from "../../api/report";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "../../styles/ReportDetail.module.css";
 
 const ReportDetailView = () => {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // 컴포넌트가 마운트된 후에 실행할 코드, 예를 들면 데이터 fetching
@@ -31,6 +34,24 @@ const ReportDetailView = () => {
 
     return `${year}년 ${month}월 ${day}일`;
   }
+
+  const handleDelete = async () => {
+    // 첫 번째 삭제 확인 팝업
+    const isConfirmed = window.confirm("이 레포트를 삭제하시겠습니까?");
+    if (isConfirmed) {
+      try {
+        // 삭제 로직 (API 호출 등)
+        await deleteReportById(id); // 예시 API 호출 함수
+        // 삭제 후 확인 팝업
+        alert("레포트가 삭제되었습니다.");
+        // 레포트 리스트 페이지로 이동
+        navigate('/report/list/' + 1);
+      } catch (error) {
+        console.error('삭제 중 오류 발생', error);
+        alert('삭제 중 오류가 발생했습니다.');
+      }
+    }
+  };
 
   // 에러가 발생했을 때 처리
   if (error) {
@@ -74,11 +95,14 @@ const ReportDetailView = () => {
           ))}
         </div>
         <div className={styles.footerContainer}>
+          <div className={styles.deleteButton} onClick={handleDelete}>삭제</div>
+          <div className={styles.createdTimeContainer}>
             <p>
               {formatCreatedTime(data.createdTime)}
               <br />
               면접자 OOO
             </p>
+          </div>
         </div>
       </div>
     );
