68971cf196ea221cd569878021b319f7d882e31a
김현수 <ekclstkfka5@gmail.com>
2023-11-13 00:26:56 +0900

    refactor: Sidebar CSS 개선

--- FE/silapmyeon/src/components/commons/Side.jsx
+++ FE/silapmyeon/src/components/commons/Side.jsx
@@ -11,6 +11,7 @@ function Side() {
   const [userNickname, setUserNickName] = useState("");
   const isLogin = useRecoilValue(IsLoginSelector);
   const [isOpen, setOpen] = useState(false);
+  const [myPageActive, setMyPageActive] = useState(false);
 
   useEffect(() => {
     const User = JSON.parse(sessionStorage.getItem("user"))?.UserAtom;
@@ -21,6 +22,10 @@ function Side() {
 
   const handleClick = () => {
     setOpen(true);
+  };
+
+  const toggleMyPage = () => {
+    setMyPageActive(!myPageActive);
   };
 
   return (
@@ -53,22 +58,25 @@ function Side() {
       <Link to={"/interview"} className="link">
         <div className="sidebarText">모의면접</div>
       </Link>
-      <div className="sidebarText">
-        마이페이지
-        <Link to={"/resume"} className="link">
-          <div className="sidebarSmallText">자소서</div>
-        </Link>
-        <Link to={"/report/list/1"} className="link">
-          <div className="sidebarSmallText">면접 리포트</div>
-        </Link>
-        <Link to={"/review"} className="link">
-          <div className="sidebarSmallText">면접 후기</div>
-        </Link>
+      <div className={`sidebarItem ${myPageActive ? 'sidebarItemActive' : ''}`}>
+        <div onClick={toggleMyPage} className="sidebarText">마이페이지</div>
+        {myPageActive && (
+          <div className="sidebarSubMenu">
+            <Link to={"/resume"} className="link">
+              <div className="sidebarSmallText">자소서</div>
+            </Link>
+            <Link to={"/report/list/1"} className="link">
+              <div className="sidebarSmallText">면접 리포트</div>
+            </Link>
+            <Link to={"/review"} className="link">
+              <div className="sidebarSmallText">면접 후기</div>
+            </Link>
+          </div>
+        )}
       </div>
       <Link to={"/community"} className="link">
         <div className="sidebarText">면접 공유</div>
       </Link>
-      <Link to="/mypage"> 테스트</Link>
     </div>
   );
 }
